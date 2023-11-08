289edffe6d23d45f55211a2743fd62677e01197d
rahulrhdev <rahulrhdev@gmail.com>
2023-11-08 10:32:24 +0530

    dsa practice

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,7 @@
-function validateEmail() {}
+function validateEmail(email) {
+    const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return regex.test(email)
+
+}
 
 module.exports = validateEmail;
